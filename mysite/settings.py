52bf6017874f0244efddb6fa38bd6f155eb50682
mariomore4 <mariomoreta710@gmail.com>
2025-06-26 20:15:38 +0200

    Añadidos archivos estáticos recopilados

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_DIRS = (os.path.join(BASE_DIR, 'nba/static'),)
 
@@ -139,3 +139,5 @@ CACHES = {
         'TIMEOUT': 3600,  # Cache por 1 hora
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
