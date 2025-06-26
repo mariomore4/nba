87738f54572256a3f7caf7cc1c9bb8c6ff91c044
mariomore4 <mariomoreta710@gmail.com>
2025-06-26 20:01:04 +0200

    Añadir dominio de Render a ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0*4a@+w++2wtiu@*_0q0=#i(9q_ef@1y#roaw%$0hffd5qysp=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nba-q0fs.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
