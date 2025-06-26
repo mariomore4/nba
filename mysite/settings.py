07f5acbe801bb903428abf11d8ccea322ec55fe4
mariomore4 <mariomoreta710@gmail.com>
2025-06-26 20:27:42 +0200

    Añadidos archivos estáticos recopilados

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = (os.path.join(BASE_DIR, 'nba/static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'nba/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
