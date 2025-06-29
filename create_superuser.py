685571db3c3730431c70dca9e66efca60759455c
mariomore4 <mariomoreta710@gmail.com>
2025-06-29 15:19:54 +0200

    Usar variable de entorno para contraseña del superusuario

--- create_superuser.py
+++ create_superuser.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+import os
 
 User = get_user_model()
 
@@ -6,7 +7,7 @@ if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser(
         username='admin',
         email='admin@mynba360.com',
-        password='admin123'
+        password=os.environ.get('DJANGO_ADMIN_PASSWORD', 'admin123')
     )
     print("✔ Superusuario creado correctamente")
 else:
